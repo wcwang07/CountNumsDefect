536e3f2a9412d3da123eba0f50ed0236448b0edf
weicwang <weicwang@cisco.com>
2018-02-26 10:20:39 -0800

    not functional count

--- Solution.java
+++ Solution.java
@@ -2,36 +2,44 @@ import java.util.Arrays;
 
 public class Solution {
 	
-	static int[][] input = {{1,0,0,1},{0,0,1,0},{0,1,0,0}, {1,0,0,0}};
+	static int[][] input = {{1,1,1,1,1},{0,0,1,0,0},{0,0,1,0,0}, {0,0,1,0,0}, {1,1,1,1,1}};
 	
 	static boolean hasNeighbor(int x, int y, int[][] arr) {
 		
 		//check north
 		if(x-1>=0 && arr[x-1][y]==1){
+			System.out.println("looking at "+x+" "+y+" ");
 			arr[x][y]=0;
 			hasNeighbor(x-1, y, arr);
 			arr[x-1][y]=0;
+			return true;
+			
 		}
 		//check east
 		if(y+1<=arr[x].length-1 && arr[x][y+1]==1){
+			System.out.println("looking at "+x+" "+y+" ");
 			arr[x][y]=0;
 			hasNeighbor(x, y+1, arr);
 			arr[x][y+1]=1;
+			return true;
 		}
 		//check south
 		if(x+1<=arr.length-1 && arr[x+1][y]==1){
+			System.out.println("looking at "+x+" "+y+" ");
 			arr[x][y]=0;
 			hasNeighbor(x+1, y, arr);
 			 arr[x+1][y]=0;
+			 return true;
 		}
 		//check west
 		if(y-1>=0 && arr[x][y-1]==1){
+			System.out.println("looking at "+x+" "+y+" ");
 			arr[x][y]=0;
 			hasNeighbor(x, y-1, arr);
 			arr[x][y-1]=0;
+			return true;
 		}
 		return false;
-
 	}
 		
 	public static void main(String[] args) {
@@ -40,9 +48,11 @@ public class Solution {
 		System.out.println(Arrays.deepToString(input));
 		//find the next defect
 		for(int i=0;i<input.length;i++) {
-			for(int j=0;j<input.length;j++) {
+			for(int j=0;j<input[i].length;j++) {
 				if(input[i][j]==1 && !hasNeighbor(i,j,input)) {
+				
 					count++;
+					
 				}
 			}
 		}
